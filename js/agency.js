10a47396f3183d134ffef377d9d1d8e9c0171e8a
Anton <m@e5t.ro>
2017-12-07 12:01:51 +0100

    Add JS stream consuming

--- js/agency.js
+++ js/agency.js
@@ -36,3 +36,42 @@
   });
 
 })(jQuery); // End of use strict
+
+// fetch() returns a promise
+var decoder = new TextDecoder();
+fetch("http://127.0.0.1:8000/catstream").then(response => {
+  // response.body is a readable stream.
+  // Calling getReader() gives access to the stream's content
+  var reader = response.body.getReader();
+  // read() returns a promise that resolves when a value has been received
+  return reader.read().then(function processResult(result) {
+    // Result objects contain two properties:
+    // done  - true/false, and value - the data
+    if (result.done) {
+      return;
+    }
+    // result.value is a Uint8Array, we need to decode it
+    var chunk = decoder.decode(result.value, {stream: true});
+    var lines = chunk.split('\n');
+    lines.forEach(function(text) {
+      if(text.length === 0) {
+        return
+      }
+      var obj = JSON.parse(text);
+        // set element attributes
+      var element = document.querySelector('.cat-gallery .portfolio-item.hide');
+      var next_element = element.cloneNode(true);
+      // add placeholder for a next cat
+      document.querySelector('.cat-gallery').appendChild(next_element);
+
+      // process and unhide the current cat
+      element.querySelector('.cat-image').setAttribute('src', obj.image);
+      element.querySelector('.cat-name').textContent = obj.name;
+      element.querySelector('.cat-price').textContent = obj.price;
+      element.classList.add('show');
+      element.classList.remove('hide');
+    });
+    // Read some more, and call this function again
+    return reader.read().then(processResult);
+  });
+});
